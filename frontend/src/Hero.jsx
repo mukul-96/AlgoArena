e28e35b928f0cc538f8d1ea100d37a956f40d7fb
Mukul . <aggarwalmukul96@gmail.com>
2024-10-19 23:43:12 +0530

    added hero

--- frontend/src/Hero.jsx
+++ frontend/src/Hero.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react"
+import {useNavigate } from "react-router-dom"
+import { BACKEND_URL } from "../config"
 export default function Hero() {
-  return (
-    <div>Hero</div>
-  )
+    const [userName, setUserName] = useState('')
+    const navigate=useNavigate()
+    const userNameHandler = (e) => {
+        setUserName(e.target.value) 
+    }
+    const clickHandler=()=>{
+        navigate(`/${userName}`)
+    }
+
+    return (
+        <div>
+            <h1>Hero</h1>
+            <label htmlFor="username">Enter username</label>
+            <input 
+                type="text" 
+                id="username" 
+                placeholder="fireZephyr" 
+                value={userName} 
+                onChange={userNameHandler} 
+            />
+            <p>Username: {userName}</p> 
+            <button onClick={clickHandler}>Submit</button>
+        </div>
+    )
 }
