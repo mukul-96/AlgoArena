ba5eb62ceefed101992324244b2f01bd5504d200
Mukul . <aggarwalmukul96@gmail.com>
2024-10-19 10:54:20 +0530

    updated backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "https://algo-arena-mukuls-projects-99b57981.vercel.app";
+export const BACKEND_URL = "https://algoarena-server.onrender.com";
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ContestArea from './ContestArea';
 import Landing from './Landing';
+import Hero from './Hero';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/:userName" element={<Landing />} />
+                <Route path="/" element={<Hero/>} />
                 <Route path="/contest/:userName/:roomID" element={<ContestArea />} />
             </Routes>
         </BrowserRouter>
