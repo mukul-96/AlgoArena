17077aed2079712278aea59e77e714638a28f580
Mukul . <aggarwalmukul96@gmail.com>
2025-05-29 01:22:31 +0530

    Update: added new feature/fixed bug

--- Backend/socket.js
+++ Backend/socket.js
@@ -59,13 +59,11 @@ const socketHandler = (io) => {
 
         socket.on('leaveRoom', ({ roomID, userName }) => {
             socket.leave(roomID);
-            localStorage.removeItem('contestStartTime');
             delete onlineUsers[userName];
             socket.to(roomID).emit('userLeft', { userName });
         });
 
         socket.on('contestResult', ({ roomID, winner }) => {
-            localStorage.removeItem('contestStartTime');
             const roomParts = roomID.split('_');
             const participant1 = roomParts[1];
             const participant2 = roomParts[2];
